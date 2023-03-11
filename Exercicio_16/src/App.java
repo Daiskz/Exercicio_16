a2d99c24ddb0cf0cb2b548c60d3632648df81d45
Daiskz <gabriel.matsubara@gmail.com>
2023-03-11 04:39:40 -0300

    Mudança no código básico

--- Exercicio_16/src/App.java
+++ Exercicio_16/src/App.java
@@ -8,8 +8,8 @@ public class App {
         double tinta = metros / 3;
         double lata = tinta / 18;
         double valor = lata * 80;
-        System.out.println("Você vai precisar de " + tinta + " litros de tinta para pintar sua parede");
-        System.out.println("Será preciso comprar " + lata + " lata(s) de tinta");
-        System.out.println("Você vai pagar " + valor + ("R$"));
+        System.out.println("Você vai precisar de " + (Math.ceil(tinta)) + " litros de tinta para pintar sua parede");
+        System.out.println("Será preciso comprar " + (Math.ceil(lata)) + " lata(s) de tinta");
+        System.out.println("Você vai pagar " + (Math.ceil(valor)) + ("R$"));
     }
 }
