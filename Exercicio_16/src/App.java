71e1ea15ed2538c71aef3aee63e80e068ddc1cee
gabriel.matsubara@gmail.com <gabriel.matsubara@gmail.com>
2023-08-11 16:31:33 -0300

    Mudanças no projeto

--- Exercicio_16/src/App.java
+++ Exercicio_16/src/App.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite quantos metros você deseja pintar: ");
-        double metros = entrada.nextDouble();
-        double tinta = metros / 3;
-        double lata = tinta / 18;
-        double valor = lata * 80;
-        System.out.println("Você vai precisar de " + (Math.ceil(tinta)) + " litros de tinta para pintar sua parede");
-        System.out.println("Será preciso comprar " + (Math.ceil(lata)) + " lata(s) de tinta");
-        System.out.println("Você vai pagar " + (Math.ceil(valor)) + ("R$"));
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Digite quantos metros você deseja pintar: ");
+            double metros = entrada.nextDouble();
+            double tinta = metros / 3;
+            double lata = tinta / 18;
+            double valor = lata * 80;
+            System.out.println("Você vai precisar de " + (Math.ceil(tinta)) + " litros de tinta para pintar sua parede");
+            System.out.println("Será preciso comprar " + (Math.ceil(lata)) + " lata(s) de tinta");
+            System.out.println("Você vai pagar " + (Math.ceil(valor)) + ("R$"));
+        }
     }
 }
